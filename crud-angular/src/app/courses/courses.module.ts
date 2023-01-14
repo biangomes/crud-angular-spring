eda4cfd641c1ae5f5ee714a3ca6933b1ea1f7486
biangomes <beanascigom@gmail.com>
2023-01-13 20:02:33 -0500

    Module with Material imports

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,10 +1,8 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import { MatTableModule } from '@angular/material/table';
-
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -13,7 +11,9 @@ import { MatTableModule } from '@angular/material/table';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
+
+
 export class CoursesModule { }
